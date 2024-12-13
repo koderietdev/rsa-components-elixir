ee5f2fdb632b3cd1211c76454a6a17d422b099bf
Harald Ringvold <harald.ringvold@gmail.com>
2024-12-13 08:23:50 +0100

    Update live_select branch to lv 1.0 support

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule RsaComponents.MixProject do
       {:tails, "~> 0.1.5"},
       {:phoenix_live_view, "~> 0.20.17"},
       # {:live_select, "~> 1.4.0"},
-      {:live_select, github: "ringvold/live_select", branch: "multi-select-tags-mode"},
+      {:live_select, github: "ringvold/live_select", branch: "quick-tags-lv-1.0"},
       # {:live_select, path: "../../live_select"},
       {:gettext, "~> 0.20"},
       {:mix_test_watch, "~> 1.2", only: [:dev, :test]},
